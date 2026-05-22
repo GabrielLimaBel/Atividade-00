53fb5758c552ef75e0d985cdf4381157f0c99771
= <=>
2026-05-22 16:01:31 -0300

    programa.py

---
+++ programa.py
@@ -0,0 +1,9 @@
+print('Bem-vindo ao sistema de cálculo de BMR(Basal Methabolic Rate).')
+print('Você deverá fornecer informações para que seja possível realizar o cálculo')
+
+peso = float(input('digite o peso em quilos: '))
+altura = float(input('digite a altura em metros: '))
+idade = int(input('digite a idade em anos: '))
+
+BMR = ((10*peso) + (6.25*altura) - (5*idade) + 5)
+print(BMR)
